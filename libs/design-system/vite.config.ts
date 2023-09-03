ae2496789fb725fe47c5451664d71764f07906b8
GauvainThery <gauvain.thery@zefir.fr>
2023-09-03 15:17:43 +0200

    feat: create design system

---
+++ libs/design-system/vite.config.ts
@@ -0,0 +1,30 @@
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'design-system',
+      formats: ['es', 'umd'],
+      fileName: (format) => `design-system.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
