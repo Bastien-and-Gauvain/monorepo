84c600d77254eda79bc2b4fb0506e8b19a12536e
GauvainThery <gauvain.thery@zefir.fr>
2023-09-10 18:17:39 +0200

    fix: get theme config from the ds config file

--- apps/linkedin-to-notion/tailwind.config.js
+++ apps/linkedin-to-notion/tailwind.config.js
@@ -1,7 +1,10 @@
+const designSystemConfig = require('../../libs/design-system/tailwind.config.js');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
   darkMode: 'class',
+  theme: designSystemConfig.theme,
   content: ['./**/*.tsx'],
   plugins: [],
 };
