3d6d32a6e3debd77f811795ddc3e4e481c9e56ab
GauvainThery <gauvain.thery@zefir.fr>
2023-09-17 19:22:43 +0200

    feat: create linkedin profile scraper

---
+++ apps/linkedin-to-notion/src/contents/linkedin-data-cleaners.ts
@@ -0,0 +1,26 @@
+/**
+ * Basic function to split first and last name in common case ie. they are split by a space
+ * @param fullName Full name eg. 'Gauvain Thery'
+ * @returns an object with a split between first and last names
+ */
+export const splitName = (fullName: string): { firstName?: string; lastName?: string } => {
+  const names = fullName.split(' ');
+  if (names.length === 1) {
+    return {
+      firstName: names[0],
+    };
+  }
+  return {
+    firstName: names[0].trim(),
+    lastName: names[names.length - 1].trim(),
+  };
+};
+
+/**
+ * Basic function to remove unwanted elements from linkedin job titles
+ * @param jobTitle The raw job title from linkedIn
+ * @returns a clean job title
+ */
+export const cleanJobTitle = (jobTitle: string): string => {
+  return jobTitle.split(/ at| @| chez/g)[0].trim();
+};
