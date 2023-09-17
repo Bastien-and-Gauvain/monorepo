04c8d12b96bec3f88cc9fa2331f82a67889ef908
Bastiche <bastien.velitchkine@live.fr>
2023-09-17 12:55:03 +0200

    💄 Created Label Comp.

---
+++ libs/design-system/src/atoms/labels/labels.tsx
@@ -0,0 +1,24 @@
+import { cn } from '../../shared/classnames';
+
+type LabelProps = {
+  type?: 'sm' | 'base';
+  inputId?: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const fontSizeMapping = {
+  sm: 'text-3.5',
+  base: 'text-4',
+};
+
+const Label = ({ type = 'base', inputId, children, className }: LabelProps) => {
+  return (
+    <label htmlFor={inputId} className={cn('antialiased', 'text-gray-600', fontSizeMapping[type], className)}>
+      {children}
+    </label>
+  );
+};
+
+export const SmallLabel = (p: LabelProps) => <Label type="sm" {...p} />;
+export const BaseLabel = (p: LabelProps) => <Label type="base" {...p} />;
