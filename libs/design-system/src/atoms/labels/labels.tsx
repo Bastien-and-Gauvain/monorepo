da0e896f940f50bda0f9790f1890e5c8b42aaaf2
Bastiche <bastien.velitchkine@live.fr>
2023-09-20 23:35:18 +0200

    📝 Documented the label props

--- libs/design-system/src/atoms/labels/labels.tsx
+++ libs/design-system/src/atoms/labels/labels.tsx
@@ -1,9 +1,27 @@
 import { cn } from '../../shared/classnames';
 
 export type LabelProps = {
+  /**
+   * A type to specify the size of the label (either small or regular).
+   * Only impacts the font size
+   */
   type?: 'sm' | 'base';
+
+  /**
+   * The id of the input the label is associated to.
+   * That how it works in html: a component has an id,
+   * and the label associated to that component has the smale id in its htmlFor attribute.
+   */
   inputId?: string;
+
+  /**
+   * The content of the label another react node when it should only be a string.
+   */
   children: React.ReactNode;
+
+  /**
+   * Some extra classes to apply to the label.
+   */
   className?: string;
 };
 
