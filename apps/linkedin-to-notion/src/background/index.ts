135e907c6c6472a9be06146340f8ab7b8abdd0b3
Bastiche <bastien.velitchkine@live.fr>
2023-10-03 18:36:20 +0200

    ⚡️ send messages only when tab update complete

--- apps/linkedin-to-notion/src/background/index.ts
+++ apps/linkedin-to-notion/src/background/index.ts
@@ -3,14 +3,16 @@ export {};
 const linkedInURLRegex = /linkedin.com\/in\/.+/;
 
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.match(linkedInURLRegex)) {
+  if (tab.url?.match(linkedInURLRegex)) {
     chrome.tabs.sendMessage(tab.id, 'toggleLinkedInNotionSidePanel');
   }
 });
 
-chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
-  if (tab.url.match(linkedInURLRegex)) {
-    return chrome.tabs.sendMessage(tabId, 'updateLinkedInNotionSidePanel');
+chrome.tabs.onUpdated.addListener((tabId, { status }, tab) => {
+  if (status === 'complete') {
+    if (tab.url?.match(linkedInURLRegex)) {
+      return chrome.tabs.sendMessage(tabId, 'updateLinkedInNotionSidePanel');
+    }
+    return chrome.tabs.sendMessage(tabId, 'closeLinkedInNotionSidePanel');
   }
-  return chrome.tabs.sendMessage(tabId, 'closeLinkedInNotionSidePanel');
 });
