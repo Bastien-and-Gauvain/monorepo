c9500ce1b10284fe7334fd67da88c1ba38a65649
Bastiche <bastien.velitchkine@live.fr>
2023-10-03 18:23:38 +0200

    🔥 commented useless code to prevent msg bugs

--- apps/linkedin-to-notion/src/background/messages/init-session.ts
+++ apps/linkedin-to-notion/src/background/messages/init-session.ts
@@ -1,15 +1,15 @@
-import type { PlasmoMessaging } from '@plasmohq/messaging';
+// import type { PlasmoMessaging } from '@plasmohq/messaging';
 
-import { supabase } from '~core/supabase';
+// import { supabase } from '~core/supabase';
 
-const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  supabase.auth.onAuthStateChange((event, session) => {
-    console.log(event, session);
-  });
+// const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
+//   supabase.auth.onAuthStateChange((event, session) => {
+//     console.log(event, session);
+//   });
 
-  await supabase.auth.setSession(req.body);
+//   await supabase.auth.setSession(req.body);
 
-  res.send({ success: true });
-};
+//   res.send({ success: true });
+// };
 
-export default handler;
+// export default handler;
