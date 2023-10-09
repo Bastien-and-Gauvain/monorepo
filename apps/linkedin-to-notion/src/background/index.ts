bd7a7498b109718a3fbcdcf9f0b64d7864179ea6
Bastiche <bastien.velitchkine@live.fr>
2023-10-09 16:57:18 +0200

    🐛 Not sending messages to pages without side pan

--- apps/linkedin-to-notion/src/background/index.ts
+++ apps/linkedin-to-notion/src/background/index.ts
@@ -2,24 +2,29 @@ export {};
 
 // You can test the regex here: https://regex101.com/r/RJgYar/1
 // Don't forget to update the comment when you update the regex on regex101.com
-const linkedInURLRegex = /linkedin\.com\/in\/[^/]+\/$/;
+const linkedInProfileRegex = /linkedin\.com\/in\/[^/]+\/$/;
+const linkedInRegex = /linkedin\.com/g;
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.match(linkedInURLRegex)) {
+  if (tab.url.match(linkedInProfileRegex)) {
     return await chrome.tabs.sendMessage(tab.id, 'toggleLinkedInNotionSidePanel');
   } else {
     return await chrome.tabs.sendMessage(tab.id, 'toggleGoBackToLinkedInSidePanel');
   }
 });
 
-chrome.tabs.onUpdated.addListener(async (tabId, { status }, tab) => {
-  if (tab.url && tab.url.match(linkedInURLRegex)) {
-    if (status === 'complete') {
+chrome.tabs.onUpdated.addListener(async (tabId, { status, url }, tab) => {
+  const urlToCheck = url || tab?.url;
+  if (urlToCheck) {
+    if (urlToCheck.match(linkedInProfileRegex) && status === 'complete') {
       return await chrome.tabs.sendMessage(tabId, 'updateLinkedInNotionSidePanel');
     }
-  } else {
-    return await chrome.tabs.sendMessage(tabId, 'closeSidePanels');
+
+    if (!urlToCheck.match(linkedInRegex)) {
+      return await chrome.tabs.sendMessage(tabId, 'closeSidePanels');
+    }
   }
+  return;
 });
 
 chrome.runtime.onMessage.addListener(async (msg) => {
