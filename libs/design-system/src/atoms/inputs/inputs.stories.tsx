e4a9119eca94f8857b23d9112081478461ae4690
Bastiche <bastien.velitchkine@live.fr>
2023-10-16 21:16:07 +0200

    🚚 Plural on toggleInput file

--- libs/design-system/src/atoms/inputs/inputs.stories.tsx
+++ libs/design-system/src/atoms/inputs/inputs.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryFn } from '@storybook/react';
 import { useState } from 'react';
 
 import { SelectInput, TextAreaInput, TextInput } from '.';
-import { ToggleInput } from './toggleInput';
+import { ToggleInput } from './toggleInputs';
 
 const meta = {
   title: 'Atoms/Inputs',
