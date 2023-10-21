72c7314785ce30649e4db01816bdc7f849e6c3d8
Bastien Vélitchkine <bvelitchkine@gmail.com>
2023-10-21 10:14:54 +0200

    ✨ feat(back) PLAS-056: Store Notion API Tokens (#33)
    Co-authored-by: GauvainThery <gauvain.thery@zefir.fr>

--- apps/linkedin-to-notion/src/background/index.ts
+++ apps/linkedin-to-notion/src/background/index.ts
@@ -1,8 +1,10 @@
+import { SecureStorage } from '@plasmohq/storage/secure';
+
 export {};
 
 // You can test the regex here: https://regex101.com/r/RJgYar/1
 // Don't forget to update the comment when you update the regex on regex101.com
-export const linkedInURLRegex = /linkedin\.com\/in\/[^/]+\/$/;
+export const linkedInURLRegex = /linkedin\.com\/in\/[^/]+\/#?$/;
 
 chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.match(linkedInURLRegex)) {
@@ -27,3 +29,16 @@ chrome.runtime.onMessage.addListener(async (msg) => {
     return await chrome.tabs.create({ url: 'https://www.linkedin.com/in/me/' });
   }
 });
+
+// TODO Should be removed, only there for temporary testing purposes
+const displayChangesInSecureStorage = async () => {
+  const storage = new SecureStorage({ area: 'local' });
+  await storage.setPassword('napoleon');
+  storage.watch({
+    notionToken: (c) => {
+      console.log('New value of notionToken: ', c.newValue);
+    },
+  });
+};
+
+displayChangesInSecureStorage();
--- apps/linkedin-to-notion/src/contents/linkedin-notion-side-panel.tsx
+++ apps/linkedin-to-notion/src/contents/linkedin-notion-side-panel.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { sendToBackground } from '@plasmohq/messaging';
 import { Storage } from '@plasmohq/storage';
 import { useStorage } from '@plasmohq/storage/hook';
+import { SecureStorage } from '@plasmohq/storage/secure';
 
 import { supabase } from '~core/supabase';
 
@@ -32,6 +33,12 @@ const LinkedinNotionSidePanel = () => {
     }),
   });
 
+  const storeEntriesSecurely = async (key: string, value: Record<string, string>): Promise<void> => {
+    const storage = new SecureStorage({ area: 'local' });
+    await storage.setPassword('napoleon');
+    await storage.set(key, value);
+  };
+
   useEffect(() => {
     async function init() {
       const { data, error } = await supabase.auth.getSession();
@@ -42,6 +49,21 @@ const LinkedinNotionSidePanel = () => {
       }
       if (data.session) {
         setUser(data.session.user);
+
+        // There's one thing we're specifically interested in in the data.session object
+        // It's the provider_token (i.e notion's api access token in this case)
+        // This token is necessary to make api calls to notion elsewhere in the app
+        // There's no way to retrieve this token if we don't save it here
+        // However, it's only available in the session object when the user logs in, not afterwards
+        // So, to make sure it's stored once and not erased afterwards, we store it in the secure storage
+        // after assessing it's in data.session
+        if (data.session.provider_token) {
+          storeEntriesSecurely('notionToken', {
+            accessToken: data.session.provider_token,
+            refreshToken: data.session.refresh_token,
+          });
+        }
+
         sendToBackground({
           name: 'sessions/resolvers/init-session',
           body: {
