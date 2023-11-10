1accb5ce334ac0cbbc55526e450b690a9726d84e
Bastiche <bastien.velitchkine@live.fr>
2023-11-10 18:22:49 +0100

    💄 Removed useless icons

--- libs/design-system/src/atoms/icons/icons.stories.tsx
+++ libs/design-system/src/atoms/icons/icons.stories.tsx
@@ -15,20 +15,4 @@ export default meta;
 
 export const Spinner: StoryFn = () => <Icon isSpinning={true} type="Spinner" className="plasmo-fill-black" />;
 
-export const ArrowRightTray: StoryFn = () => <Icon type="ArrowRightTray" className="plasmo-stroke-black" />;
-
-export const ChevronLeftRight: StoryFn = () => <Icon type="ChevronLeftRight" className="plasmo-stroke-black" />;
-
-export const ChevronDown: StoryFn = () => <Icon type="ChevronDown" className="plasmo-stroke-black" />;
-
-export const XMark: StoryFn = () => <Icon type="XMark" className="plasmo-stroke-black" />;
-
-export const CheckCircle: StoryFn = () => <Icon type="CheckCircle" className="plasmo-stroke-black" />;
-
-export const InformationCircle: StoryFn = () => <Icon type="InformationCircle" className="plasmo-stroke-black" />;
-
-export const ExclamationCircle: StoryFn = () => <Icon type="ExclamationCircle" className="plasmo-stroke-black" />;
-
-export const ArrowPath: StoryFn = () => <Icon type="ArrowPath" className="plasmo-stroke-black" />;
-
 export const SixDots: StoryFn = () => <Icon type="SixDots" className="plasmo-stroke-black" />;
--- libs/design-system/src/atoms/icons/svg/index.ts
+++ libs/design-system/src/atoms/icons/svg/index.ts
@@ -1,24 +1,8 @@
-import { ArrowPath } from './ArrowPath';
-import { ArrowRightTray } from './ArrowRightTray';
-import { CheckCircle } from './CheckCircle';
-import { ChevronDown } from './ChevronDown';
-import { ChevronLeftRight } from './ChevronLeftRight';
-import { ExclamationCircle } from './ExclamationCircle';
-import { InformationCircle } from './InformationCircle';
 import { SixDots } from './SixDots';
 import { Spinner } from './Spinner';
-import { XMark } from './XMark';
 
 export const icons = {
   Spinner,
-  ArrowRightTray,
-  InformationCircle,
-  CheckCircle,
-  ArrowPath,
-  ChevronLeftRight,
-  ChevronDown,
-  XMark,
-  ExclamationCircle,
   SixDots,
 };
 
