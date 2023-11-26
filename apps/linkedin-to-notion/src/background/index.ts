721f25de0102820c5cb13f7a9e166fb012ae2375
GauvainThery <79575726+GauvainThery@users.noreply.github.com>
2023-11-26 20:13:47 +0100

    🎨 impr(back) 67: prevent google api max write operations per minute (#69)

--- apps/linkedin-to-notion/src/background/index.ts
+++ apps/linkedin-to-notion/src/background/index.ts
@@ -4,19 +4,42 @@ export {};
 // Don't forget to update the comment when you update the regex on regex101.com
 export const linkedInURLRegex = /linkedin\.com\/in\/[^/]+\/#?/;
 
-chrome.tabs.onUpdated.addListener(async (tabId, { status }, tab) => {
-  if (!tab?.url?.match(linkedInURLRegex)) {
-    return await chrome.tabs.sendMessage(tabId, 'closeSidePanels');
+chrome.tabs.onUpdated.addListener(async (tabId, { status }, tab): Promise<void> => {
+  if (status !== 'complete') {
+    return;
   }
 
-  await chrome.tabs.sendMessage(tabId, 'openLinkedInNotionSidePanel');
-  if (status === 'complete') {
-    return await chrome.tabs.sendMessage(tabId, 'updateLinkedInNotionSidePanel');
+  const isLinkedInURL = tab?.url?.match(linkedInURLRegex);
+
+  if (!isLinkedInURL) {
+    try {
+      await chrome.tabs.sendMessage(tabId, 'closeSidePanels');
+    } catch (e) {
+      console.log('Error closing side panels', e);
+    }
+  }
+
+  if (isLinkedInURL) {
+    try {
+      await chrome.tabs.sendMessage(tabId, 'openLinkedInNotionSidePanel');
+    } catch (e) {
+      console.log('Error opening side panels', e);
+    }
+
+    try {
+      await chrome.tabs.sendMessage(tabId, 'updateLinkedInNotionSidePanel');
+    } catch (e) {
+      console.log('Error updating side panels', e);
+    }
   }
 });
 
 chrome.runtime.onMessage.addListener(async (msg) => {
   if (msg === 'openLinkedInTab') {
-    return await chrome.tabs.create({ url: 'https://www.linkedin.com/in/me/' });
+    try {
+      await chrome.tabs.create({ url: 'https://www.linkedin.com/in/me/' });
+    } catch (e) {
+      console.log('Error opening LinkedIn tab', e);
+    }
   }
 });
