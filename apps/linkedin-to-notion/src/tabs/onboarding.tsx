1c3f09d4d4c7bc15ae7f78c5d80c613aa83a6935
GauvainThery <gauvain.thery@zefir.fr>
2023-12-11 23:18:21 +0100

    Refactor onboarding page and update task count

--- apps/linkedin-to-notion/src/tabs/onboarding.tsx
+++ apps/linkedin-to-notion/src/tabs/onboarding.tsx
@@ -2,18 +2,50 @@ import { ButtonPrimary, Card, Heading1, Heading2 } from 'design-system';
 
 import '~style.css'; // mandatory to inject tailwindcss styles
 
+import type { Provider } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
 import recruitivity_icon from 'url:./../../assets/icon.png';
 import linkedin_logo from 'url:./../../assets/linkedin_logo.svg';
 import notion_logo from 'url:./../../assets/notion_logo.svg';
 import pin_icon from 'url:./../../assets/pin_icon.svg';
 
-export const DeltaFlyerPage = () => {
-  const [tasks, setTasks] = useState<number>(3);
+import { useStorage } from '@plasmohq/storage/hook';
+
+import { supabase } from '~core/supabase';
+import { OnboardingStatus } from '~src/background/messages/users/services/user.service';
+import type { Tables } from '~src/background/types/supabase';
+
+export const OnboardingPage = () => {
+  const [numberOfTasks, setNumberOfTasks] = useState<number>(3);
+  const [user] = useStorage<Tables<'users'>>('user');
+  console.log('🔥 ・ OnboardingPage ・ user:', user);
 
   useEffect(() => {
-    setTasks(3);
-  }, []);
+    if (!user) {
+      return;
+    }
+
+    const onboardingStatus = user.onboarding_status;
+    if (onboardingStatus === OnboardingStatus.CONNECTED_TO_NOTION) {
+      setNumberOfTasks(2);
+    }
+    if (onboardingStatus === OnboardingStatus.FIRST_PROFILE_SAVED) {
+      setNumberOfTasks(1);
+    }
+    if (onboardingStatus === OnboardingStatus.EXTENSION_PINNED) {
+      setNumberOfTasks(0);
+    }
+  }, [user]);
+
+  const handleOAuthLogin = async (provider: Provider, scopes?: string) => {
+    await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: 'chrome-extension://nepaaeacmgpjpeighkbfmgacoinooale/tabs/onboarding.html',
+        scopes,
+      },
+    });
+  };
 
   return (
     <div className="plasmo-bg-background-light plasmo-h-screen plasmo-w-screen">
@@ -25,14 +57,15 @@ export const DeltaFlyerPage = () => {
       <div className="plasmo-bg-background-dark plasmo-rounded-b-[236px] plasmo-h-[350px] plasmo-w-full plasmo-absolute" />
       <div className="plasmo-flex plasmo-flex-col plasmo-items-center plasmo-absolute plasmo-z-10 xl:plasmo-pt-28 md:plasmo-pt-20 plasmo-p-12 plasmo-gap-12 plasmo-w-full">
         <Heading1 color="plasmo-text-white" className="plasmo-text-center plasmo-px-16">
-          {tasks} SIMPLE STEPS AWAY FROM NEVER COPY PASTING INFORMATION FROM LINKEDIN AGAIN.
+          {numberOfTasks} SIMPLE STEP{numberOfTasks > 1 ? 'S' : ''} AWAY FROM NEVER COPY PASTING INFORMATION FROM
+          LINKEDIN AGAIN.
         </Heading1>
         <div className="plasmo-grid plasmo-grid-cols-1 md:plasmo-grid-cols-2 xl:plasmo-grid-cols-3 plasmo-gap-12 plasmo-items-center">
           <Card
             className="plasmo-bg-background-light"
             title={<Heading2>1 - Connect to Notion</Heading2>}
             icon={<img src={notion_logo} alt="Notion Logo" width={150} height={150} />}
-            callToAction={<ButtonPrimary>Connect to Notion</ButtonPrimary>}
+            callToAction={<ButtonPrimary onClick={() => handleOAuthLogin('notion')}>Connect to Notion</ButtonPrimary>}
             state="current"
           />
           <Card
@@ -54,4 +87,4 @@ export const DeltaFlyerPage = () => {
   );
 };
 
-export default DeltaFlyerPage;
+export default OnboardingPage;
