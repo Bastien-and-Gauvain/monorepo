bafdd6163ebf427a7590d397f8b12e7158cd82a4
GauvainThery <gauvain.thery@zefir.fr>
2023-12-12 23:29:28 +0100

    💄 Update BasicLayout component height

--- apps/linkedin-to-notion/src/components/Layouts/BasicLayout.tsx
+++ apps/linkedin-to-notion/src/components/Layouts/BasicLayout.tsx
@@ -9,7 +9,7 @@ export const BasicLayout = ({ children }: { children: ReactNode }) => {
         alt="Recruitivity Logo"
         className="plasmo-absolute plasmo-top-0 plasmo-left-0 plasmo-m-9 plasmo-w-12 plasmo-h-12 plasmo-z-20"
       />
-      <div className="plasmo-bg-background-dark plasmo-rounded-b-[236px] plasmo-h-[350px] plasmo-w-full plasmo-absolute" />
+      <div className="plasmo-bg-background-dark plasmo-rounded-b-[236px] plasmo-h-[750px] md:plasmo-h-[500px] xl:plasmo-h-[350px] plasmo-w-full plasmo-absolute" />
       <div className="plasmo-flex plasmo-flex-col plasmo-items-center plasmo-absolute plasmo-z-10 xl:plasmo-pt-28 md:plasmo-pt-20 plasmo-p-12 plasmo-gap-12 plasmo-w-full">
         {children}
       </div>
