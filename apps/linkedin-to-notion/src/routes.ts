8d23f5aeddca6e8dd65780e55c9f5401cb4d7b98
GauvainThery <gauvain.thery@zefir.fr>
2023-12-15 23:40:54 +0100

    🧱 Add onboarding.html to web_accessible_resources and update routes

---
+++ apps/linkedin-to-notion/src/routes.ts
@@ -0,0 +1,8 @@
+export const routes = {
+  tabs: {
+    onboarding: `${process.env.PLASMO_PUBLIC_CRX_PREFIX}${process.env.PLASMO_PUBLIC_CRX_ID}/tabs/onboarding.html`,
+  },
+  linkedin: {
+    me: 'https://www.linkedin.com/in/me/',
+  },
+};
