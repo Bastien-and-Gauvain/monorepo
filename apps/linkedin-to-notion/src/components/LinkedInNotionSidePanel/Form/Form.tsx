91bc344151e92b328b2fdb189c539b9859f083cd
Bastiche <bastien.velitchkine@live.fr>
2023-12-23 19:13:21 +0100

    ✨ Increment nb profiles saved when profl. saved

--- apps/linkedin-to-notion/src/components/LinkedInNotionSidePanel/Form/Form.tsx
+++ apps/linkedin-to-notion/src/components/LinkedInNotionSidePanel/Form/Form.tsx
@@ -3,7 +3,7 @@ import type { Session } from '@supabase/supabase-js';
 import { SelectEntry, TextAreaEntry, TextEntry, ToggleEntry } from 'design-system';
 import { useEffect, useState } from 'react';
 
-import { sendToBackground } from '@plasmohq/messaging';
+import { sendToBackground, type MessagesMetadata } from '@plasmohq/messaging';
 import { useStorage } from '@plasmohq/storage/hook';
 
 import type {
@@ -145,6 +145,18 @@ export const Form = ({
     return;
   };
 
+  const incrementNumberProfilesSaved = async () => {
+    try {
+      await sendToBackground({
+        name: 'users/resolvers/incrementNumberProfilesSaved' as keyof MessagesMetadata,
+        body: { id: user.id },
+      });
+    } catch (error) {
+      console.log("Couldn't increment the number of profiles saved", error);
+      return;
+    }
+  };
+
   const saveLinkedInProfile = async (): Promise<void> => {
     setIsSaveLoading(true);
 
@@ -165,6 +177,7 @@ export const Form = ({
         },
       });
       console.log('Profile saved in Notion');
+      incrementNumberProfilesSaved();
       setCurrentNotionValues(fromInputsToNotionProfileInformation(formValues, res.id, res.url));
       setAlertState('profile-saved');
     } catch (error) {
