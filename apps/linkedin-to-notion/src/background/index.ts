020bf3a5b79158114ecdaf65f77bd48d60c30ad9
GauvainThery <gauvain.thery@zefir.fr>
2024-02-11 14:00:11 +0100

    🐛 Fix sidepanel opening

--- apps/linkedin-to-notion/src/background/index.ts
+++ apps/linkedin-to-notion/src/background/index.ts
@@ -4,7 +4,7 @@ export {};
 
 // You can test the regex here: https://regex101.com/r/RJgYar/2
 // Don't forget to update the comment when you update the regex on regex101.com
-export const linkedInProfileURLRegex = /linkedin\.com\/in\/[^/]+\/#?/;
+export const linkedInProfileURLRegex = /linkedin\.com\/in\/[^/]+\/#?$/;
 export const linkedInURLRegex = /linkedin\.com/;
 
 chrome.tabs.onUpdated.addListener(async (tabId, { status }, tab): Promise<void> => {
@@ -12,22 +12,35 @@ chrome.tabs.onUpdated.addListener(async (tabId, { status }, tab): Promise<void> 
     return;
   }
 
-  const isOnLinkedIn = tab?.url?.match(linkedInURLRegex);
-  const isOnLinkedInProfile = tab?.url?.match(linkedInProfileURLRegex);
+  if (!tab?.url) {
+    return;
+  }
+
+  const isOnLinkedIn = linkedInURLRegex.test(tab.url);
+  const isOnLinkedInProfile = linkedInProfileURLRegex.test(tab.url);
 
   // We close the extension when we leave LinkedIn
   if (!isOnLinkedIn) {
     try {
-      console.log(tab?.url);
+      console.log(tab.url);
       await chrome.tabs.sendMessage(tabId, 'closeSidePanels');
     } catch (e) {
       console.log('Error closing side panels', e);
     }
   }
 
+  if (!isOnLinkedInProfile) {
+    try {
+      await chrome.tabs.sendMessage(tabId, 'closeSidePanels');
+    } catch (e) {
+      console.log('Error updating side panels', e);
+    }
+  }
+
   // We update the side panel's content when we are on a LinkedIn profile
   if (isOnLinkedInProfile) {
     try {
+      await chrome.tabs.sendMessage(tabId, 'openLinkedInNotionSidePanel');
       await chrome.tabs.sendMessage(tabId, 'updateLinkedInNotionSidePanel');
     } catch (e) {
       console.log('Error updating side panels', e);
@@ -39,7 +52,9 @@ chrome.runtime.onMessage.addListener(async (msg) => {
   if (msg === 'openLinkedInTab') {
     try {
       const updatedTab = await chrome.tabs.update({ url: 'https://www.linkedin.com/in/me/' });
-      await chrome.tabs.sendMessage(updatedTab.id, 'openLinkedInNotionSidePanel');
+      if (updatedTab?.id) {
+        chrome.tabs.sendMessage(updatedTab.id, 'openLinkedInNotionSidePanel');
+      }
     } catch (e) {
       console.log('Error opening LinkedIn tab', e);
     }
